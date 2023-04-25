63cc88f3494aa23bb6bc2bf0453fabacee38468f
mugilragavi <mugilshiva26@gmail.com>
2023-04-25 12:03:00 +0530

    first commit

---
+++ src/Components/Footer.js
@@ -0,0 +1,46 @@
+import React from 'react';
+
+
+const Footer = () => {
+  return (
+    <div>
+      <footer class="footer-distributed">
+
+<div class="footer-left">
+    <h3>Quote<span>Generator</span></h3>
+
+    
+
+    <p class="footer-company-name">Copyright © 2021 <strong>Mugilragavi</strong> All rights reserved</p>
+</div>
+
+<div class="footer-center">
+    <div>
+        <i class="fa fa-map-marker"></i>
+        <p><span>Coimbatore</span>
+            Tamilnadu</p>
+    </div>
+
+    <div>
+        <i class="fa fa-phone"></i>
+        <p>+91 9042807335</p>
+    </div>
+    <div>
+        <i class="fa fa-envelope"></i>
+        <p><a href="mailto:sagar00001.co@gmail.com">mugilshiva26@gmail.com</a></p>
+    </div>
+</div>
+<div class="footer-right">
+    <p class="footer-company-about">
+        <span>About Us</span>
+        <strong>RANDOM QUOTE GENERATOR-</strong>If you're looking for some motivational quotes or inspirational quotes to positively 
+        influence your day, you've come to the right place. 
+        This is exactly what the Random Quotes Generator is able to do.
+    </p>
+  
+</div>
+</footer>
+    </div>
+  );
+};
+export default Footer;
---
+++ src/Components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+const Header = () => {
+  return (
+   <div class="header">
+    <h1>RANDOM QUOTE GENERATOR</h1>
+   </div>
+  );
+};
+export default Header;
